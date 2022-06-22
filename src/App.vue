2a16dd43ec195fe8464f430f02d18559fa4b34d6
Kian_ <tianjianen2015@gmail.com>
2022-06-22 18:01:57 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <el-button type="primary">hello</el-button>
+   <el-card shadow="always"> Always </el-card>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
